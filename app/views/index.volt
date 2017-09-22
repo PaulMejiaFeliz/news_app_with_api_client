4542d08d33c1998b5293ebd7bdefe4541e17747c
PaulMejiaFeliz <paulmejiafeliz@gmail.com>
2017-09-22 10:01:45 -0400

    Change files mode to 644

--- app/views/index.volt
+++ app/views/index.volt
@@ -1,39 +1,39 @@
 <!DOCTYPE html>
-<html lang='en'>
+<html lang="en">
 <head>
-    <meta charset='UTF-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {{ getTitle() }}
     {{ stylesheetLink('/bootstrap/css/bootstrap.css') }}
     {{ stylesheetLink('/css/site.css') }}
 </head>
 <body>
-    <nav class='navbar navbar-default'>
-        <div class='container-fluid'>
-            <div class='navbar-header'>
-                <button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1' aria-expanded='false'>
-                    <span class='sr-only'>Menu</span>
-                    <span class='icon-bar'></span>
-                    <span class='icon-bar'></span>
-                    <span class='icon-bar'></span>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Menu</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
                 </button>
-                <a class='navbar-brand' href='/'>News App</a>
+                <a class="navbar-brand" href="/">News App</a>
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 {% if (session.has('user')) %}
-                <ul class='nav navbar-nav'>
+                <ul class="nav navbar-nav">
                     <li> {{ linkTo('/news/add', 'New Post') }} </li>
                     <li> {{ linkTo('/myNews', 'My Posts') }} </li>
                 
                 </ul>
-                <ul class='nav navbar-nav navbar-right'>
+                <ul class="nav navbar-nav navbar-right">
                     {% set user = session.get('user') %}
                     <li class='navbar-text'>{{user['name']}} {{user['lastName']}}</li>
                     <li> {{ linkTo('/logout', 'Logout') }} </li>
                 </ul>
                 {% else %}
-                <ul class='nav navbar-nav navbar-right'>                
+                <ul class="nav navbar-nav navbar-right">                
                     <li> {{ linkTo('/register', 'Register') }} </li>
                     <li> {{ linkTo('/login', 'Login') }} </li>
                 </ul>
@@ -41,7 +41,7 @@
             </div>
         </div>
     </nav>
-    <div class='container'>
+    <div class="container">
         {{ content() }}
     </div>
     {{ javascriptInclude('https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js', false) }}
--- cache/_home_newsapp_app_views_index.volt.php
+++ cache/_home_newsapp_app_views_index.volt.php
@@ -1,39 +1,39 @@
 <!DOCTYPE html>
-<html lang='en'>
+<html lang="en">
 <head>
-    <meta charset='UTF-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?= $this->tag->gettitle() ?>
     <?= $this->tag->stylesheetlink('/bootstrap/css/bootstrap.css') ?>
     <?= $this->tag->stylesheetlink('/css/site.css') ?>
 </head>
 <body>
-    <nav class='navbar navbar-default'>
-        <div class='container-fluid'>
-            <div class='navbar-header'>
-                <button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1' aria-expanded='false'>
-                    <span class='sr-only'>Menu</span>
-                    <span class='icon-bar'></span>
-                    <span class='icon-bar'></span>
-                    <span class='icon-bar'></span>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Menu</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
                 </button>
-                <a class='navbar-brand' href='/'>News App</a>
+                <a class="navbar-brand" href="/">News App</a>
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <?php if (($this->session->has('user'))) { ?>
-                <ul class='nav navbar-nav'>
+                <ul class="nav navbar-nav">
                     <li> <?= $this->tag->linkto('/news/add', 'New Post') ?> </li>
                     <li> <?= $this->tag->linkto('/myNews', 'My Posts') ?> </li>
                 
                 </ul>
-                <ul class='nav navbar-nav navbar-right'>
+                <ul class="nav navbar-nav navbar-right">
                     <?php $user = $this->session->get('user'); ?>
                     <li class='navbar-text'><?= $user['name'] ?> <?= $user['lastName'] ?></li>
                     <li> <?= $this->tag->linkto('/logout', 'Logout') ?> </li>
                 </ul>
                 <?php } else { ?>
-                <ul class='nav navbar-nav navbar-right'>                
+                <ul class="nav navbar-nav navbar-right">                
                     <li> <?= $this->tag->linkto('/register', 'Register') ?> </li>
                     <li> <?= $this->tag->linkto('/login', 'Login') ?> </li>
                 </ul>
@@ -41,7 +41,7 @@
             </div>
         </div>
     </nav>
-    <div class='container'>
+    <div class="container">
         <?= $this->getContent() ?>
     </div>
     <?= $this->tag->javascriptinclude('https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js', false) ?>
